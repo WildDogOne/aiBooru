f3853455298b2c27da3f8d8aede9d67587ac4dd8
Läubli Linus (laei) <laei@zhaw.ch>
2023-03-14 17:36:25 +0100

    adjusted unittests

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -5,7 +5,7 @@ from content.functions import get_images, read_description_file, hash_file
 
 class Test(TestCase):
     def test_get_images(self):
-        for x, y in get_images("../tests", logdir="../data/"):
+        for x, y in get_images("../tests", force=False, logdir="../data/"):
             self.assertEqual("./test.jpg", x)
             self.assertEqual("./test.yaml", y)
 
@@ -13,9 +13,10 @@ class Test(TestCase):
         actual = read_description_file("./test.yaml")
         expected = ('s', 'black_box')
         self.assertEqual(expected, actual)
+
     def test_hash_file(self):
         directory = "../tests"
         file = "./test.jpg"
         actual = hash_file(directory, file, logdir="../data/")
-        expected = 1
+        expected = False
         self.assertEqual(expected, actual)
