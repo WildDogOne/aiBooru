3004259bb81bccb0183ffc16e3cec98f7114d592
Läubli Linus (laei) <laei@zhaw.ch>
2023-03-26 11:02:56 +0200

    refactored into smaller functions

--- content/tagger.py
+++ content/tagger.py
@@ -6,28 +6,21 @@ import tensorflow as tf
 from PIL import Image
 
 
-def evaluate_image(input_image: str = None, model_url: str = None, project: str = None, threshold: float = None):
-    if model_url is None:
-        model_url = "https://github.com/KichangKim/DeepDanbooru/releases/download/v3-20211112-sgd-e28/deepdanbooru-v3-20211112-sgd-e28.zip"
-    if project is None:
-        project = "./deepbooru"
-    if threshold is None:
-        threshold = 0.5
-    model_zip = project + "/model.zip"
-    if not os.path.exists(project):
-        os.makedirs(project)
-    if not os.path.exists(model_zip):
-        r = requests.get(model_url, allow_redirects=True)
-        open(model_zip, 'wb').write(r.content)
-    if not os.path.exists(project + "/project.json"):
-        import zipfile
+def download_model(model_url, model_zip):
+    r = requests.get(model_url, allow_redirects=True)
+    with open(model_zip, 'wb') as f:
+        f.write(r.content)
 
-        with zipfile.ZipFile(model_zip, 'r') as zip_ref:
-            zip_ref.extractall(project)
 
+def extract_model(model_zip, project):
+    import zipfile
+
+    with zipfile.ZipFile(model_zip, 'r') as zip_ref:
+        zip_ref.extractall(project)
+
+
+def prepare_image(input_image, model):
     pil_image = Image.open(input_image)
-    tags = dd.project.load_tags_from_project(project)
-    model = dd.project.load_model_from_project(project, compile_model=False)
 
     width = model.input_shape[2]
     height = model.input_shape[1]
@@ -43,27 +36,46 @@ def evaluate_image(input_image: str = None, model_url: str = None, project: str 
     image = image / 255.0
     image_shape = image.shape
     image = image.reshape((1, image_shape[0], image_shape[1], image_shape[2]))
+    return image
 
+
+def evaluate_image(input_image: str = None, model_url: str = None, project: str = None, threshold: float = None):
+    if model_url is None:
+        model_url = "https://github.com/KichangKim/DeepDanbooru/releases/download/v3-20211112-sgd-e28/deepdanbooru-v3-20211112-sgd-e28.zip"
+    if project is None:
+        project = "./deepbooru"
+    if threshold is None:
+        threshold = 0.5
+    model_zip = os.path.join(project, "model.zip")
+
+    if not os.path.exists(project):
+        os.makedirs(project)
+    if not os.path.exists(model_zip):
+        download_model(model_url, model_zip)
+    if not os.path.exists(os.path.join(project, "project.json")):
+        extract_model(model_zip, project)
+
+    tags = dd.project.load_tags_from_project(project)
+    model = dd.project.load_model_from_project(project, compile_model=False)
+
+    image = prepare_image(input_image, model)
     y = model.predict(image)[0]
 
     result_dict = {}
 
     for i, tag in enumerate(tags):
         result_dict[tag] = y[i]
+
     result_tags_out = []
-    # result_tags_print = []
     result_tags = []
     rating = "s"
+
     for tag in tags:
         if result_dict[tag] >= threshold:
             if tag.startswith("rating:"):
                 rating = tag
                 continue
             result_tags_out.append(tag.replace('_', ' ').replace(':', ' '))
-            # result_tags_print.append(f'{result_dict[tag]} {tag}')
             result_tags.append({tag.replace('_', ' ').replace(':', ' '): result_dict[tag]})
-
-    # print('\n'.join(sorted(result_tags_print, reverse=True)))
-    # pprint(result_tags_print)
 
     return result_tags_out, rating
